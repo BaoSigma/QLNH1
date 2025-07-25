09aba215ecfa79a719994a2388e9f6d89bbbbd73
dongkhanh208 <khanhdgtv00309@gmail.com>
2025-07-25 10:10:18 +0700

    Merge origin/master
    # Conflicts:
    #	src/poly/cafe/icons/clear.png
    #	src/poly/cafe/icons/read.png
    #	src/poly/cafe/icons/update.png

--- src/DAO/impl/ChiTietHoaDonImpl.java
+++ src/DAO/impl/ChiTietHoaDonImpl.java
@@ -4,11 +4,101 @@
  */
 package DAO.impl;
 
+import DAO.ModelDAO.ChiTietHoaDonDAO;
+import Model.ChiTietHoaDon;
+import Util.UJdbc;
+import Util.UQuery;
+import java.util.List;
+
 /**
  *
  * @author baoha
  */
-public class ChiTietHoaDonImpl {
-    public static final String sqlcreate = "EXEC sp_ThemChiTietHoaDon ?, ?, ?, ?, ?, NULL;";
-    
+
+public class ChiTietHoaDonImpl implements ChiTietHoaDonDAO {
+
+    private static final String sqlInsert = """
+        EXEC sp_ThemChiTietHoaDon ?, ?, ?, ?, ?, ?;
+    """;
+
+    private static final String sqlUpdate = """
+        UPDATE ChiTietHoaDon
+        SET SoLuong = ?, GhiChu = ?, TrangThai = ?, MaVanDon = ?
+        WHERE MaHD = ? AND MaMon = ?;
+    """;
+
+    private static final String sqlDelete = """
+        DELETE FROM ChiTietHoaDon
+        WHERE MaHD = ? AND MaMon = ?;
+    """;
+
+    private static final String sqlFindAll = """
+        SELECT MaHD, MaMon, SoLuong, GhiChu, TrangThai, MaVanDon FROM ChiTietHoaDon;
+    """;
+
+    private static final String sqlFindById = """
+        SELECT MaHD, MaMon, SoLuong, GhiChu, TrangThai, MaVanDon
+        FROM ChiTietHoaDon
+        WHERE MaHD = ? AND MaMon = ?;
+    """;
+
+    @Override
+    public ChiTietHoaDon create(ChiTietHoaDon ct) {
+        Object[] values = {
+            ct.getMaHD(),
+            ct.getMaMon(),
+            ct.getSoLuong(),
+            ct.getGhiChu(),
+            ct.getTrangThai(),
+            ct.getMaVanDon()
+        };
+        UJdbc.executeUpdate(sqlInsert, values);
+        return ct;
+    }
+
+    @Override
+    public void update(ChiTietHoaDon ct) {
+        Object[] values = {
+            ct.getSoLuong(),
+            ct.getGhiChu(),
+            ct.getTrangThai(),
+            ct.getMaVanDon(),
+            ct.getMaHD(),
+            ct.getMaMon()
+        };
+        UJdbc.executeUpdate(sqlUpdate, values);
+    }
+
+    @Override
+    public void deleteById(Object id) {
+        if (id instanceof String[] ids && ids.length == 2) {
+            UJdbc.executeUpdate(sqlDelete, ids[0], ids[1]);
+        } else {
+            throw new IllegalArgumentException("ID phải là String[2] gồm MaHD và MaMon");
+        }
+    }
+
+    @Override
+    public List<ChiTietHoaDon> findAll() {
+        return UQuery.getBeanList(ChiTietHoaDon.class, sqlFindAll);
+    }
+
+    @Override
+    public ChiTietHoaDon findById(Object id) {
+        if (id instanceof String[] ids && ids.length == 2) {
+            return UQuery.getSingleBean(ChiTietHoaDon.class, sqlFindById, ids[0], ids[1]);
+        }
+        return null;
+    }
+
+    public List<ChiTietHoaDon> findByMaHD(String maHD) {
+        String sql = """
+            SELECT MaHD, MaMon, SoLuong, GhiChu, TrangThai, MaVanDon
+            FROM ChiTietHoaDon
+            WHERE MaHD = ?;
+        """;
+        return UQuery.getBeanList(ChiTietHoaDon.class, sql, maHD);
+    }
 }
+
+
